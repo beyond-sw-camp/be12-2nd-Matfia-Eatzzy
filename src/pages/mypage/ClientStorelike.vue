3de042f2f174e721c2f7ffd6541a0ba12ae481ec
taehoon0518 <cjsxogns0518@gmail.com>
2024-12-31 21:54:48 +0900

    [Style] 고객 좋아요 페이지 스타일 수정

--- src/pages/mypage/ClientStorelike.vue
+++ src/pages/mypage/ClientStorelike.vue
@@ -43,7 +43,7 @@ table tr:first-child th {
   font-size: 1.25rem;
 }
 .col1 {
-  width: 40%;
+  width: 30%;
   min-width: 10%;
 }
 .col2 {
@@ -51,7 +51,7 @@ table tr:first-child th {
   min-width: 10%;
 }
 .col3 {
-  width: 20%;
+  width: 30%;
   min-width: 10%;
 }
 .col4 {
