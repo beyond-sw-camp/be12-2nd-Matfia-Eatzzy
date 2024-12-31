2be9191567c03c89586816901c2c7c0768a5bc3c
yeon7485 <wsx4379@naver.com>
2024-12-31 17:01:40 +0900

    [Feature] 점주 상품 조회 스타일링, 수정 구현

--- src/stores/useProductsStore.js
+++ src/stores/useProductsStore.js
@@ -12,6 +12,17 @@ export const useProductsStore = defineStore("products", {
       expirationDate: "",
     },
     products: [],
+    modifyProduct: {
+      idx: 0,
+      name: "",
+      price: 0,
+      stock: 0,
+      expiration_date: "",
+      cooking_time: 0,
+      description: "",
+      category: "",
+      image: "",
+    },
   }),
   actions: {
     async getProductBestList() {
@@ -37,67 +48,56 @@ export const useProductsStore = defineStore("products", {
     },
 
     async addProduct() {
-      const response = await axios.post(
-        "/ysh/product/add",
-        {
-          name: "pizza",
-          price: 16000,
-          stock: 1,
-          expiration_date: "2024-12-25",
-          cooking_time: 10,
-          category: "한식",
-          description: "delicious pizza",
-          image: "피자사진",
-        }
-      );
+      const response = await axios.post("/ysh/product/add", {
+        name: "pizza",
+        price: 16000,
+        stock: 1,
+        expiration_date: "2024-12-25",
+        cooking_time: 10,
+        category: "한식",
+        description: "delicious pizza",
+        image: "피자사진",
+      });
       return response.data;
     },
 
     async ModifyProduct() {
-      const response = await axios.post(
-        "/ysh/product/modify",
-        {
-          name: "pizza",
-          price: 18000,
-          stock: 3,
-          description: "delicious pizza",
-          image: "피사사진",
-        }
-      );
+      const response = await axios.post("/ysh/product/modify", {
+        name: "pizza",
+        price: 18000,
+        stock: 3,
+        description: "delicious pizza",
+        image: "피사사진",
+      });
       return response.data;
     },
 
     async RegisterDelivery() {
-      const response = await axios.post(
-        "/ysh/product/delivery",
-        {
-          courier_company: "한진택배",
-          tracking_number: 69012481,
-          order_id: "ord2",
-        }
-      );
+      const response = await axios.post("/ysh/product/delivery", {
+        courier_company: "한진택배",
+        tracking_number: 69012481,
+        order_id: "ord2",
+      });
       return response.data;
     },
 
     async getSellerProduct() {
-      const response = await axios.get(
-        "/ysh/seller/product"
-      );
+      const response = await axios.get("/ysh/seller/product");
       return response.data;
     },
 
     async getOrderProduct() {
-      const response = await axios.get(
-        "/ysh/order/product"
-      );
+      const response = await axios.get("/ysh/order/product");
       return response.data;
     },
 
     async getOrderDetails() {
-      const response = await axios.get(
-        "/ysh/order/details"
-      );
+      const response = await axios.get("/ysh/order/details");
       return response.data;
+    },
+
+    setModifyProduct(productData) {
+      this.modifyProduct = productData;
     },
   },
 });
