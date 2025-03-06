846f1efea2d8017ea1ebc0e6232e3405db0ca37c
sangmi Park <miyad927@naver.com>
2025-03-06 19:36:44 +0900

    관리자 페이지 관련 추가가

---
+++ src/pages/admin/Admin.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import AdminHeader from "./components/AdminHeader.vue";
+  import AdminLeft from "./components/AdminLeft.vue";
+</script>
+
+<template>
+  <div class="page_admin">
+    <AdminLeft />
+    <div class="admin_body_layout">
+      <AdminHeader />
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
